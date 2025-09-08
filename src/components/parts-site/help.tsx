0aa6b951e4eddbf6293cdbb192d5debced2981ec
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2025-09-08 11:27:53 -0300

    feat: help
    commit branch section p residents

--- src/components/parts-site/help.tsx
+++ src/components/parts-site/help.tsx
@@ -9,7 +9,7 @@ export const Help = () =>{
             src={imgBanner.src}
             className='border-2 border-amber-600'
           />
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem distinctio, nihil hic quo consectetur modi numquam accusamus rem quia necessitatibus debitis laborum perspiciatis quidem cupiditate, voluptatibus magni culpa a harum ea, consequatur pariatur alias? Rerum, distinctio? Molestias nisi inventore, maxime iure corporis, ab suscipit totam similique fugit officia cum eum aut corrupti autem sapiente non. Voluptas culpa architecto nesciunt enim praesentium quam eaque itaque officia, perferendis aliquam facilis et reprehenderit labore atque quasi sequi! Repudiandae esse, suscipit qui nihil dolorem repellat aut eveniet cumque dolores debitis a? Vel autem, officia, necessitatibus et accusantium quidem explicabo laboriosam, vero quia illum tempore.</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem distinctio, nihil hic quo consectetur modi gni culpa a harum ea, consequatur pariatur alias? Rerum, distinctio? Molestias nisi inventore, maxime iure corporis, ab suscipit totam similique fugit officia cum eum aut corrupti autem sapiente non. Voluptas culpa architecto nesciunt enim praesentium quam eaque itaque officia, perferendis aliquam facilis et reprehenderit labore atque quasi sequi! Repudiandae esse, suscipit qui nihil dolorem repellat aut eveniet cumque dolores debitis a? Vel autem, officia, necessitatibus et accusantium quidem explicabo laboriosam, vero quia illum tempore.</p>
         </div>
       </div>
     </section>
