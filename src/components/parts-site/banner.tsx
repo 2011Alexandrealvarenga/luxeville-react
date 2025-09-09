b47551b70f11625db381801fcb5a4cf0161bb88d
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2025-09-09 14:06:13 -0300

    feat: branch banner
    commit branch banner

---
+++ src/components/parts-site/banner.tsx
@@ -0,0 +1,19 @@
+import banner_topo from '../../assets/img/first-house.png';
+export const Banner = () =>{
+  return(
+    <section id="banner" className="bg_bege_claro">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2">
+          <div className="content">
+            <h1 className="title font-bold">Trusted Real <br/>Estate Solutions</h1>
+            <p>Discover premium properties tailored to your lifstyle.</p>
+            <p>From buying to renting, we make deals simple.</p>
+            <p>Your perfect home is just a click away.</p>
+            <button className="btn bg_marrow_escuro text-white px-4 py-1 rounded-md text-sm cursor-pointer">Click here</button>
+          </div>
+          <img src={banner_topo.src} className="img-banner" />
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import { Help } from "@/components/parts-site/help";
 import { Testimonials } from "@/components/parts-site/testemonials";
 import { Contact } from "@/components/parts-site/contact";
 import { Footer } from "@/components/parts-site/footer";
+import { Banner } from "@/components/parts-site/banner";
 
 // import imgXd from '../assets/img/XD PARA TESTAR.jpg';
 
 export default function Home() {
   return (
    <>
+   <Banner />
    <Help/>
    <P_residents />
    <Testimonials/>
